d827c3476b68b93846138820a3ede49436a0f9a3
Mo0nX8 <anakinka2323@gmail.com>
2024-12-09 17:54:29 +0100

    Created Android Application and MVC Application

---
+++ Webshop/WebshopWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebshopWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
