89c8dd13eddb5ea05925ba1b55396079038fafae
Mo0nX8 <anakinka2323@gmail.com>
2025-02-13 19:00:34 +0100

    the start of the Product view

---
+++ Webshop/WebshopWeb/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Webshop.EntityFramework;
+
+namespace WebshopWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        private GlobalDbContext _context;
+
+        public ProductController(GlobalDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_context.StorageData.ToList());
+        }
+    }
+}
