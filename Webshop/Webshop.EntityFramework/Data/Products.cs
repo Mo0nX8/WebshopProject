99d9359fbca99da8fb4424cde638fc45b15b2928
Mo0nX8 <anakinka2323@gmail.com>
2025-02-13 18:28:38 +0100

    Started products.json for data seeding

--- Webshop/Webshop.EntityFramework/Data/Products.cs
+++ Webshop/Webshop.EntityFramework/Data/Products.cs
@@ -4,9 +4,10 @@
     {
         public int Id { get; set; }
         public string ProductName { get; set; }
-        public int ProductCount { get; set; }
+        public int Quanity { get; set; }
         public int Price { get; set; }
         public string[] Tags { get; set; }
+        public string[] Description { get; set; }
 
 
         public ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
