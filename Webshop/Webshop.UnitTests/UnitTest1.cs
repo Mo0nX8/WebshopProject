95ac31e2165dc4a64dc70b66b55e8ce9d8461f3b
Mo0nX8 <anakinka2323@gmail.com>
2025-02-24 08:25:54 +0100

    UnitTest proj added to project

---
+++ Webshop/Webshop.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Webshop.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
