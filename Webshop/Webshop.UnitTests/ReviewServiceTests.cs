caaa80b8310ebd89a587c2878c21e8c4ed1fd0d8
Mo0nX8 <anakinka2323@gmail.com>
2025-03-30 18:40:35 +0200

    ReviewService Tests

---
+++ Webshop/Webshop.UnitTests/ReviewServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Webshop.EntityFramework.Data;
+using Webshop.EntityFramework.Managers.Reviews;
+
+namespace Webshop.UnitTests
+{
+    [TestFixture]
+    public class ReviewServiceTests
+    {
+        private Mock<IReviewRepository> _reviewRepositoryMock;
+        private IReviewManager _reviewManager;
+
+        [SetUp]
+        public void Setup()
+        {
+            _reviewRepositoryMock = new Mock<IReviewRepository>();
+            _reviewManager=new ReviewManager( _reviewRepositoryMock.Object );
+        }
+
+        [Test]
+        public void TestThatAddReviewShouldCallAddReviewOnce()
+        {
+            var review = new Review
+            {
+                Id = 1,
+                ProductId = 12,
+                Comment = "Test",
+                Rating = 4,
+                CreatedAt = DateTime.Now,
+            };
+            _reviewManager.AddReview(review);
+
+            _reviewRepositoryMock.Verify(r => r.AddReview(review),Times.Once);
+        }
+    }
+}
